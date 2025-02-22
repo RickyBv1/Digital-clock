00320dca1ed80381ae397ac2115fc402c4f9fb9a
RickyBv1 <rickycortes668668@gmail.com>
2025-02-22 00:14:20 -0500

    Add real-time updating functionality to digital clock and enhance styles with glow effect

--- index.js
+++ index.js
@@ -19,3 +19,28 @@ function setNumber(i, value) {
         segment.children[0].classList.toggle("on", numbersMap[value][j])
     })
 }
+
+setInterval(() => {
+    updatedTime();
+}, 1000);
+
+function getTwoDigits(number) {
+    if(number.length === 2) return number;
+    return "0" + number;
+}
+
+function updatedTime(){
+    const now = new Date();
+    const hours = getTwoDigits(now.getHours().toString());
+    const minutes = getTwoDigits(now.getMinutes().toString());
+    const seconds = getTwoDigits(now.getSeconds().toString());
+    
+    setNumber(0, hours[0])
+    setNumber(1, hours[1])
+    setNumber(2, minutes[0])
+    setNumber(3, minutes[1])
+    setNumber(4, seconds[0])
+    setNumber(5, seconds[1])
+}
+
+updatedTime();
